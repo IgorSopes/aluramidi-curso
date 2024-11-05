52c14b028b2f4a478ed34eba2c564b0a0c7f320c
IgorSopes <sopesigor@gmail.com>
2024-11-05 17:35:08 -0300

    adicionado sons as teclas

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (_idElementAudio) {
+    document.querySelector(_idElementAudio).play();
 }
 
+let contador = 0;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-const listaDeTeclas = document.querySelectorAll('tecla')
+while(contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+    contador = contador + 1
+    console.log(contador)
+}
